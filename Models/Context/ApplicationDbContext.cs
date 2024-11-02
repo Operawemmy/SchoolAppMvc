8b1fd998afed04c513b56f8c2fe400ef4f6d5885
OPERAKENNY <OPERAKENNY@DESKTOP-NRH8E3I>
2024-11-02 15:26:43 +0000

    Add project files.

---
+++ Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Models.Entities;
+
+namespace SchoolApp.Models.Context
+{
+    public class ApplicationDbContext : DbContext // DbContext is a class from EFCore.
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // setting up domain classes or entities
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Models/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolApp.Models.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!; // this means the name of department is not nullable.
+        public string? Description { get; set; }
+        public string Code { get; set; } = default!;
+    }
+}
